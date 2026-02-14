3d36c31ab0d0943caa49578a1a0c0195e2129830
SunLab-UGA <142451941+SunLab-UGA@users.noreply.github.com>
2026-02-14 21:42:08 +0000

    Deploying to gh-pages from @ SunLab-UGA/sunlab@1ce66e057d016b4fbbcd42fe21b61a2bd0b92198 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://sunlab-uga.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://sunlab-uga.github.io/sunlab//#contact">contact us</a>.'
               }
             ]
           }
