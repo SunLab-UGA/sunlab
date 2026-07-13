89c8893b9dc140d42ac47287c4323863905a8239
ethanzhu96 <ethanzhu987@gmail.com>
2026-07-13 16:02:59 -0400

    search bar fix

--- assets/js/search-setup.js
+++ assets/js/search-setup.js
@@ -7,11 +7,18 @@ if (searchTheme === "dark") {
   ninjaKeys.classList.remove("dark");
 }
 
-const openSearchModal = () => {
+const openSearchModal = async () => {
   // collapse navbarNav if expanded on mobile
   const $navbarNav = $("#navbarNav");
   if ($navbarNav.hasClass("show")) {
     $navbarNav.collapse("hide");
   }
+
+  await customElements.whenDefined("ninja-keys");
   ninjaKeys.open();
 };
+
+const searchToggle = document.querySelector("#search-toggle");
+if (searchToggle) {
+  searchToggle.addEventListener("click", openSearchModal);
+}
